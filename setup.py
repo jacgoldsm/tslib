fed1e46250f12c9e1e62ce9309567e579c67edfb
jacgoldsm <jacgoldsm@reed.edu>
2023-11-24 12:25:20 -0800

    extras

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup, find_packages
 
 
 reqs = ["pandas"]
-extra_reqs = ["pyspark"]
 setup(
     packages=find_packages(),
     python_requires=">=3.7",
     install_requires=reqs,
-    extras_require=extra_reqs,
     name="tslib",
     version="0.0.1",
     description="Complete and consistent API for Time-Series functionality",
